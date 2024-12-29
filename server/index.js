b2cc647c535e3b6e96de8bed86aa3e961cfff016
hezou <hezoubeke18@gmail.com>
2024-12-29 17:09:39 +0000

    update

--- server/index.js
+++ server/index.js
@@ -16,6 +16,36 @@ app.get('/' ,(req ,res) => {
     .catch(err => res.json(err))
 })
 
+app.post('/create',  (req, res) =>{
+    UserModel.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.json(err))
+})
+
+app.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('Updating user with ID:', id, 'and data:', req.body); // Debug log
+    UserModel.findByIdAndUpdate(
+        { _id: id },
+        {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+        },
+        { new: true } // Return the updated document
+    )
+        .then((user) => res.json(user))
+        .catch((err) => res.json(err));
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete(id)
+        .then(() => res.json({ message: 'User deleted successfully' }))
+        .catch((err) => res.json(err));
+});
+
+
 
 app.listen(3001, () => {
     console.log("Server is running ");
